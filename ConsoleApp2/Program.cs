4ec4f00391dc7697a5af7a48c0a77a1f74499821
marina kanjuga <marinakanjuga@gmail.com>
2019-11-18 00:00:03 +0100

    uncomited by code.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cas19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+           //da prodje kroz petlju 10 puta i da sabere sve vrednosti tog brojaca, suma brojaca, da krene od 1
+            for (int a = 1; a <= 10; a++) 
+            {
+                Console.WriteLine(a);
+                suma = suma + a;             
+            }
+          
+            Console.WriteLine(suma);
+            Console.ReadKey();
+        }
+       
+    }
+}
+
+
