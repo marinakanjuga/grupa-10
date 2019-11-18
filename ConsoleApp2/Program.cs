8f8d84fa5c2a77bfb1003c30823c329d0da6d136
marina kanjuga <marinakanjuga@gmail.com>
2019-11-18 12:06:59 +0100

    Cover null colesing

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,18 +10,29 @@ namespace Cas19
     {
         static void Main(string[] args)
         {
-            int suma = 0;
-           //da prodje kroz petlju 10 puta i da sabere sve vrednosti tog brojaca, suma brojaca, da krene od 1
-            for (int a = 1; a <= 10; a++) 
+            double? num = null;
+            double? num1 = 3.15;
+            double? numr;
+
+            numr = num ?? 5;//numr ce biti num, a u slucaju da je num null, onda ce biti 5 (ili moze num1)
+
+            if (num == null) //ovo je zamena za num ??
             {
-                Console.WriteLine(a);
-                suma = suma + a;             
+                numr = num1;
             }
-          
-            Console.WriteLine(suma);
-            Console.ReadKey();
-        }
-       
+            else 
+            {
+                numr = num;
+            }
+
+            //ternarni operator
+            numr = (num < 10) ? 10 : num1;
+            
+            
+            
+            Console.WriteLine("rezulatat {0}", numr);
+            Console.ReadKey();          
+        }    
     }
 }
 
